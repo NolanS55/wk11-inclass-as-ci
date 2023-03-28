a66ecf0070ca2b38393a87704c0e7f029a1e73e4
NolanS55 <nsmith55@myseneca.ca>
2023-03-28 15:42:45 -0400

    added test2

--- tests/practice.test.js
+++ tests/practice.test.js
@@ -4,28 +4,23 @@ import { render } from '@testing-library/react';
 
 describe('some tests', () => {
     test("renders at least one link to https://github.com in the first child of 'main' ", () => {
-  const {container} = render(<Home />);
+        const {container} = render(<Home />);
 
-  // attempt to fetch the first child element within the "main" element
-  const child = container.querySelector('main *');
-
-  // ensure the the child element exists
-  expect(child).toBeTruthy();
 
   // get all "a" elements within the child element
-  const childLinks = child.querySelectorAll("a");
+        const childLinks = container.querySelectorAll("a");
 
   // ensure there is at least 1 "a" element within the child
-  expect(childLinks.length).toBeGreaterThan(0); // at least one link
+        expect(childLinks.length).toBeGreaterThan(0); // at least one link
 
-  let githubLinks = 0;
+        let githubLinks = 0;
 
-  childLinks.forEach(link => {
-    if(link.href.includes("https://github.com"))
-      githubLinks++;
-  });
+        childLinks.forEach(link => {
+            if(link.href.includes("https://github.com"))
+            githubLinks++;
+        });
 
-  // ensure that there is at least one link that includes "https://vercel.com"
-  expect(vercelLinks).toBeGreaterThan(0); // at least one vercel link
+  // ensur that there is at least one link that includes "https://vercel.com"
+        expect(githubLinks).toBeGreaterThan(0); // at least one vercel linke
 });
 });

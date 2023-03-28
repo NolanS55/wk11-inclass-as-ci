61e0a659e40353f306e244316f06684e4812d838
NolanS55 <nsmith55@myseneca.ca>
2023-03-28 16:07:32 -0400

    added test6

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,9 @@
-import Layout from "components/Layout";
+
 import Image from "next/image";
 
 export default function Home() {
   return (
-    <Layout>
+      <>
       <h1>
         <span style={{ marginRight: ".3em", verticalAlign: "middle" }}>
           <Image src="/GitHub-Mark-32px.png" width="32" height="32" alt="" />
@@ -54,6 +54,6 @@ export default function Home() {
           margin-bottom: 0.5rem;
         }
       `}</style>
-    </Layout>
+    </>
   );
 }
--- tests/index.test.js
+++ tests/index.test.js
@@ -1,13 +1,25 @@
-import Home from '@/pages/index';
+import Home from '../pages/index';
 import '@testing-library/jest-dom';
 import { render } from '@testing-library/react';
 
 describe('some tests', () => {
-      test("renders at least one link to https://github.com in the first child of 'main' ", () => {
-      
-            let a = 1
-            expect(a).toBeGreaterThan(0)
+      test("renders at least one", () => {
+            const {container} = render(<Home />);
     
-      
+      // get all "a" elements within the child element
+            const childLinks = container.querySelectorAll("a");
+    
+      // ensure there is at least 1 "a" element within the child
+            expect(childLinks.length).toBeGreaterThan(0); // at least one link
+    
+            let githubLinks = 0;
+    
+            childLinks.forEach(link => {
+                if(link.href.includes("https://github.com"))
+                githubLinks++;
+            });
+    
+      // ensur that there is at least one link that includes "https://vercel.com"
+            expect(githubLinks).toBeGreaterThan(0); // at least one vercel linke
     });
 });
